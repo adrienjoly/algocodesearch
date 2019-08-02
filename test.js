bf0667256d21e776fef24be6a83cbfdfa4c201ec
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-08-02 15:43:30 +0200

    feat: fetch and display references of a symbol (2nd working LSP request)

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
-const { createServerSocketTransport, createProtocolConnection, InitializeRequest } = require('vscode-languageserver');
+const {
+  createServerSocketTransport,
+  createProtocolConnection,
+  InitializeRequest,
+  ReferencesRequest,
+} = require('vscode-languageserver');
 
 const LANGUAGE_SERVER_PORT = 2089; // e.g. javascript-typescript-langserver
 
 (async () => {
 
-  const [ input, output ] = createServerSocketTransport(LANGUAGE_SERVER_PORT, 'utf-8');
+  const [input, output] = createServerSocketTransport(LANGUAGE_SERVER_PORT, 'utf-8');
   //output.write({ jsonrpc: 'hello' });
 
   const connection = await createProtocolConnection(input, output, console);
@@ -16,12 +21,27 @@ const LANGUAGE_SERVER_PORT = 2089; // e.g. javascript-typescript-langserver
   await connection.listen();
 
   const initRes = await connection.sendRequest(InitializeRequest.type, {
-    rootUri: 'file:///home/dirkb',
+    rootUri: `file://${__dirname}`, // current directory
     processId: 1,
     capabilities: {},
     workspaceFolders: null,
+    trace: "verbose"
   });
   console.log({ initRes });
+
+  const refsRes = await connection.sendRequest(ReferencesRequest.type, {
+    textDocument: {
+      uri: `file://${__dirname}/test.js`
+    },
+    position: {
+      line: 1, // starts at zero
+      character: 4 // starts at zero
+    },
+    context: {
+      includeDeclaration: true
+    }
+  });
+  console.log('=>', JSON.stringify(refsRes, null, 2));
 
   // close the connection and exit
   await connection.dispose();
