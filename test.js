a40f07c911e3392f57036b5f2aab1ec873ed3c72
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-08-02 14:56:27 +0200

    first commit: client can send a request to language server

---
+++ test.js
@@ -0,0 +1,30 @@
+const { createServerSocketTransport, createProtocolConnection, InitializeRequest } = require('vscode-languageserver');
+
+const LANGUAGE_SERVER_PORT = 2089; // e.g. javascript-typescript-langserver
+
+(async () => {
+
+  const [ input, output ] = createServerSocketTransport(LANGUAGE_SERVER_PORT, 'utf-8');
+  //output.write({ jsonrpc: 'hello' });
+
+  const connection = await createProtocolConnection(input, output, console);
+
+  connection.onNotification(function () {
+    console.log('Notification', arguments);
+  });
+
+  await connection.listen();
+
+  const initRes = await connection.sendRequest(InitializeRequest.type, {
+    rootUri: 'file:///home/dirkb',
+    processId: 1,
+    capabilities: {},
+    workspaceFolders: null,
+  });
+  console.log({ initRes });
+
+  // close the connection and exit
+  await connection.dispose();
+  process.exit(0);
+
+})();
