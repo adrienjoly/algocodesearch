3d8b8fb636ada69383f4496295c762cec1b3ebf7
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-09-06 14:19:15 +0200

    fixed CLI, using JavaScriptIndexer

--- indexing.js
+++ indexing.js
@@ -1,32 +1,20 @@
+const path = require('path');
+const JavaScriptIndexer = require('./JavaScriptIndexer');
 
 const { writeFile } = require('fs');
 const { promisify } = require('util');
 
-const repodir = process.argv[2] || "../javascript-typescript-langserver/src";
+const repodir = path.resolve(process.argv[2] || "../javascript-typescript-langserver/src");
 
 (async () => {
-    // Generate 2 json files
-    const promiseWriteFile = promisify(writeFile);
-    await promiseWriteFile("index-symbols-v2.json", JSON.stringify(symbols, null, 2));
-    await promiseWriteFile("index-refs-v2.json", JSON.stringify(symbolRefs, null, 2));
-  
- process.exit(0);
+  const indexer = new JavaScriptIndexer(repodir);
+  const symbols = await indexer.indexSymbols();
+  const refs = await indexer.indexRefs(symbols);
 
+  // Generate 2 json files
+  const promiseWriteFile = promisify(writeFile);
+  await promiseWriteFile("index-symbols-v2.json", JSON.stringify(symbols, null, 2));
+  await promiseWriteFile("index-refs-v2.json", JSON.stringify(refs, null, 2));
+
+  process.exit(0);
 })();
-
-const symbols = [];
-const symbolRefs = [];
-
-const crypto = require('crypto');
-
-const hash = str => crypto.createHash('md5').update(str).digest("hex")
-
-function addSymbol(symbol) {
-  const symbolID = hash(JSON.stringify(symbol));
-  symbols.push({ objectID: symbolID, ...symbol });
-}
-
-function addSymbolRef(symbol, ref) {
-  const symbolID = hash(JSON.stringify(symbol));
-  symbolRefs.push({ symbolID, ...ref });
-}
