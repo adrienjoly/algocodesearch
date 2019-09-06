54643758c58f9770f642dccf77fbfaca7a68cea7
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-09-06 14:32:20 +0200

    refacto: re-use paths in test file

--- test.js
+++ test.js
@@ -1,33 +1,37 @@
 /* global describe it expect */
 
+const path = require('path');
 const JavaScriptIndexer = require('./JavaScriptIndexer');
+
+const REL_REPO_PATH = "./test-repo";
+const ABS_REPO_PATH = path.resolve(REL_REPO_PATH);
 
 describe('indexing a javascript file', () => {
 
   it('should not allow relative paths', async () => {
-    expect(() => new JavaScriptIndexer("./test-repo")).toThrow();
+    expect(() => new JavaScriptIndexer(REL_REPO_PATH)).toThrow();
   })
 
   it('should allow absolute paths', async () => {
-    expect(() => new JavaScriptIndexer(`${__dirname}/test-repo`)).not.toThrow();
+    expect(() => new JavaScriptIndexer(ABS_REPO_PATH)).not.toThrow();
   })
 
   it('returns 1 symbol', async () => {
-    const indexer = new JavaScriptIndexer(`${__dirname}/test-repo`);
+    const indexer = new JavaScriptIndexer(ABS_REPO_PATH);
     const symbols = await indexer.indexSymbols();
 
     expect(symbols).toHaveLength(1);
   })
 
   it('should give IDs to symbols', async () => {
-    const indexer = new JavaScriptIndexer(`${__dirname}/test-repo`);
+    const indexer = new JavaScriptIndexer(ABS_REPO_PATH);
     const symbols = await indexer.indexSymbols();
 
     expect(symbols[0]).toHaveProperty('symbolID');
   })
 
   it('returns 2 refs to that symbol', async () => {
-    const indexer = new JavaScriptIndexer(`${__dirname}/test-repo`);
+    const indexer = new JavaScriptIndexer(ABS_REPO_PATH);
     const symbols = await indexer.indexSymbols();
     const refs = await indexer.indexRefs(symbols);
 
@@ -35,7 +39,7 @@ describe('indexing a javascript file', () => {
   })
 
   it('should reference symbolIDs in refs', async () => {
-    const indexer = new JavaScriptIndexer(`${__dirname}/test-repo`);
+    const indexer = new JavaScriptIndexer(ABS_REPO_PATH);
     const symbols = await indexer.indexSymbols();
     const refs = await indexer.indexRefs(symbols);
 
